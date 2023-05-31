0129c0c8c2e04679fa73e93d68bd094d683c30c4
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-05-31 17:15:26 +0300

    adding constants and commands

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-
-
+const { PORT } = require('./constants')
 
 const server = express()
+
+server.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
