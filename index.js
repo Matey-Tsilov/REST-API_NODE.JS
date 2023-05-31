494a6c1cbbd92c466019a96ff9c003c3fac33ec9
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-05-31 17:48:00 +0300

    basic user and one collection router

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require('express')
 const { PORT } = require('./constants')
 
 const server = express()
+server.use(express.json())
+
 
 server.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
