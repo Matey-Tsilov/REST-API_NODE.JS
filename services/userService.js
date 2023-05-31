8e6db8276e0e52fe46829b8a01b7e54cbf4e8af2
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-05-31 18:35:09 +0300

    add jwt library

--- services/userService.js
+++ services/userService.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../constants");
+
 // exports.register = (userData) =>  
 // exports.login = (email, password) => 
 // exports.logout = (id) =>  
