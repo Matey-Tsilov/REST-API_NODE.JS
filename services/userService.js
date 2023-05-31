a335a5b7f643fb81aa2b2382763a4418f1154576
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-05-31 18:37:50 +0300

    add generate token function

--- services/userService.js
+++ services/userService.js
@@ -5,3 +5,19 @@ const { SECRET } = require("../constants");
 // exports.register = (userData) =>  
 // exports.login = (email, password) => 
 // exports.logout = (id) =>  
+
+exports.generateToken = (user) => {
+    const payload = { _id: user._id, username: user.username, email: user.email };
+    const options = {expiresIn: '2d'}
+  
+    const tokenPromise = new Promise((resolve, reject) => {
+      jwt.sign(payload, SECRET, options, (err, decodedToken) => {
+          if (err) {
+             return reject(err)
+          }
+  
+          resolve(decodedToken)
+      });
+  })
+  return tokenPromise
+} 
