34f6c1207b04aaa29d15cd519c8fd3edf0b6a7e6
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-06-01 14:48:56 +0300

    initial start compressed in a single async function - "start"

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const CORSMiddleware = require('./middlewares/headersMiddleware')
-const { PORT } = require('./constants')
+const { PORT, CONNECTION_STRING } = require('./constants')
 
-const server = express()
-server.use(CORSMiddleware)
-server.use(express.json())
-//dataBase init
-const { dbinit } = require('./config/dbConfig')
-dbinit()
 
-server.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
+async function start() {
+    try {
+        const db = await mongoose.connect(CONNECTION_STRING)
+        console.log("DB is ready");
+    } catch (error) {
+        console.log('Error connecting to DB');
+        return process.exit(1)
+    }
+
+    const server = express()
+
+    server.use(CORSMiddleware)
+    server.use(express.json())
+
+    server.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
+}
+
+start()
