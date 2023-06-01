a5b6a1386330f71f70f0eb142e560150208ef624
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-06-01 14:33:22 +0300

    add CORS errors eliminator - custom headers

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { PORT } = require('./constants')
+const headersMiddleware = require('./middlewares/headersMiddleware')
 
 const server = express()
+server.use(headersMiddleware)
 server.use(express.json())
 //dataBase init
 const { dbinit } = require('./config/dbinit')
 dbinit()
-
 
 server.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
