90d06c21f6ee9f93e2d6d94ad0ca4ca275a8a0b9
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-06-01 19:19:35 +0300

    add errorHandling to random collection controller

--- controllers/collectionController.js
+++ controllers/collectionController.js
@@ -1,19 +1,32 @@
 const router = require('express').Router()
 const collectionService = require('../services/collectionService')
-//сложи всичко в try catchs!!!
+
 router.get('/catalog', async (req, res) => {
-    const items = await collectionService.getAll()
-    res.json(items)
+    try {
+        const items = await collectionService.getAll()
+        res.json(items)
+    } catch (error) {
+        res.status(404).json({message: 'Failed to load ressources'})
+    }
 })
 router.post('/catalog', async (req, res) => {
     const body = req.body
-    const createdItem = await collectionService.create(body)
-    res.status(201).json(createdItem)
+    try {
+        const createdItem = await collectionService.create(body)
+        res.status(201).json(createdItem)
+    } catch (error) {
+        res.status(404).json({message: 'Failed to load ressources'})
+    }
 })
 router.get('/catalog/:id', async (req, res) => {
     const id = req.params.id
-    const item = await collectionService.getById(id)
-    res.json(item)
+    try {
+        const item = await collectionService.getById(id)
+        res.json(item)
+    } catch (error) {
+        res.status(404).json({message: 'Failed to load ressources'})
+    }
+    
 })
 
 module.exports = router
