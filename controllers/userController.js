af94ac5209241c07555b1af6c1a4982ece9af45e
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-06-02 16:32:52 +0300

    implement User controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,32 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const userService = require("../services/userService");
+const mongooseErrorMapper = require('../utils/mongooseErrorMapper')
 
-router.post('/login', (req, res) => {
-    const body = req.body
-    //Find the record in DB if there, if not throw error
-    res.json({})
-})
-router.post('/register', (req, res) => {
-    const body = req.body
-    //Create new record in DB
-    res.json({})
-})
-router.get('/logout/:id', (req, res) => {
-    //delete record from DB
-    req.json({})
-})
+router.post("/login", async (req, res) => {
+  const {email, password} = req.body;
+  
+  try {
+    const session = await userService.login(email, password)
+    res.json(session);
+  } catch (error) {
+    const errorMsg = mongooseErrorMapper(error);
+    res.status(404).json({ message: errorMsg });
+  }
+});
+router.post("/register", async (req, res) => {
+  const body = req.body;
 
-module.exports = router
+  try {
+    const newUser = await userService.register(body);
+    res.json(newUser);
+  } catch (error) {
+    const errorMsg = mongooseErrorMapper(error);
+    res.status(404).json({ message: errorMsg });
+  }
+});
+router.get("/logout/:id", (req, res) => {
+  //delete record from DB
+  req.json({});
+});
+
+module.exports = router;
